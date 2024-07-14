70218b7ef0dbc96741a0e1ff9efbdf13902a77ea
joanescolanuria <joanescolanuria@gmail.com>
2024-07-14 19:15:51 +0200

    asdasd

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,9 @@ const nextConfig = {
     assetPrefix: '/blog01/',
     basePath: '/blog01',
     trailingSlash: true,
-   
+    images: {
+      unoptimized: true,
+    },
   };
   
   export default nextConfig;
