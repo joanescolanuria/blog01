08252396c24d6849f307e1bc9f3c56ab1d12ce7a
joanescolanuria <joanescolanuria@gmail.com>
2024-07-14 19:35:37 +0200

    a21

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'useClient'
-import Header from "@/components/Header";
-import BlogList from "@/components/BlogList";
-import Footer from "@/components/Footer";
+import Header from "../components/Header"
+import BlogList from "../components/BlogList";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
