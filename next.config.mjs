a98c798e2113c3dabd78238934317655aaae5a23
joanescolanuria <joanescolanuria@gmail.com>
2024-07-14 18:42:08 +0200

    add003

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',  // Añadir esta línea para exportar los archivos estáticos
+    output: 'export',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/blog01/' : '',
     trailingSlash: true,
+    images: {
+      unoptimized: true,
+    },
   };
   
   export default nextConfig;
